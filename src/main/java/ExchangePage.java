547b5ce3bbc8f56961df53c372fc2406a646ece7
tomskih <tomskih.e@gmail.com>
2020-04-12 00:56:49 +0500

    solution

--- src/main/java/ExchangePage.java
+++ src/main/java/ExchangePage.java
@@ -1,15 +1,26 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selenide.$$;
+
 public class ExchangePage {
-    public SelenideElement headerBlock = (SelenideElement) By.xpath("//div[@data-block-type='headerSlim']");
+    public SelenideElement headerBlock = Selenide.$(By.xpath("//div[@data-block-type='headerSlim']"));
+    public SelenideElement logo = headerBlock.$(By.xpath("./div/div/a[@href='/']"));
+    public ElementsCollection headerLinks= headerBlock.$$(By.tagName("a")).filterBy(Condition.visible);
 
     public ExchangePage open(){
         Selenide.open("/");
         return this;
     }
+
+    public ExchangePage back(){
+        Selenide.back();
+        return this;
+    }
+
     public void isExistElement(SelenideElement element){
         element.shouldBe(Condition.visible);
     }
--- src/test/java/ExchangeTest.java
+++ src/test/java/ExchangeTest.java
@@ -1,18 +1,39 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class ExchangeTest {
     ExchangePage exchangePage = new ExchangePage();
 
     @BeforeClass
-    public void openPage(){
+    public static void setUp(){
+        Configuration.baseUrl = "https://www.tinkoff.ru/about/exchange";
+        ExchangePage exchangePage = new ExchangePage();
         exchangePage.open();
     }
 
 
-    @Test
-    public void isExistsPageElements(){
+    @Test //Проверить, что загрузился хэдер со всеми основными элементами. Здесь же проверить доступность всех ссылок
+    public void isExistsPageElements() {
+        int i = 0;
         exchangePage.isExistElement(exchangePage.headerBlock);
+        exchangePage.isExistElement(exchangePage.logo);
+
+        while (i < exchangePage.headerLinks.size()) {   //просто прокликиваем по всем видимым ссылкам и проверяем наличие хедера
+            exchangePage.headerLinks.get(i).click();
+            exchangePage.headerBlock.shouldBe(Condition.exist);
+            exchangePage.open();
+            i++;
+        }
+
+        //нужно ещё прокликать скрытые
+
     }
 
 }
