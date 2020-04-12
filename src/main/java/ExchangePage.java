e3d69d3e57f568421b4dd6308c49426d49824dac
tomskih <tomskih.e@gmail.com>
2020-04-12 17:50:11 +0500

    solution

--- src/main/java/ExchangePage.java
+++ src/main/java/ExchangePage.java
@@ -1,29 +1,15 @@
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$$;
 
-public class ExchangePage {
-    public SelenideElement headerBlock = Selenide.$(By.xpath("//div[@data-block-type='headerSlim']"));
-    public SelenideElement logo = headerBlock.$(By.xpath("./div/div/a[@href='/']"));
-    public ElementsCollection headerLinks= headerBlock.$$(By.tagName("a")).filterBy(Condition.visible);
+public class ExchangePage extends Page{
 
-    public ExchangePage open(){
-        Selenide.open("/");
+    @Override
+    public Page open(){
+        Selenide.open("https://www.tinkoff.ru/about/exchange/");
         return this;
     }
 
-    public ExchangePage back(){
-        Selenide.back();
-        return this;
-    }
-
-    public void isExistElement(SelenideElement element){
-        element.shouldBe(Condition.visible);
-    }
 
     public class CurrencySelect {
         public By CurrencySelect = By.xpath("//div[@data-qa-file='CurrencySelects']");
--- src/test/java/ExchangeTest.java
+++ src/test/java/ExchangeTest.java
@@ -2,38 +2,63 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.sun.jndi.toolkit.url.Uri;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.net.MalformedURLException;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class ExchangeTest {
     ExchangePage exchangePage = new ExchangePage();
+    ExchangePage.Header header = new ExchangePage.Header();
+    ExchangePage.Footer footer = new ExchangePage.Footer();
+//    protected WebDriver driver;
 
     @BeforeClass
-    public static void setUp(){
-        Configuration.baseUrl = "https://www.tinkoff.ru/about/exchange";
+    public static void setUp() {
+//        ChromeDriverManager.getInstance().setup();
+        Configuration.baseUrl = "https://www.tinkoff.ru";
         ExchangePage exchangePage = new ExchangePage();
+
+    //1. Перейти на https://www.tinkoff.ru/about/exchange/ (страница курса валют в tinkoff.ru)
         exchangePage.open();
     }
 
-
-    @Test //Проверить, что загрузился хэдер со всеми основными элементами. Здесь же проверить доступность всех ссылок
+   @Test
     public void isExistsPageElements() {
         int i = 0;
-        exchangePage.isExistElement(exchangePage.headerBlock);
-        exchangePage.isExistElement(exchangePage.logo);
-
-        while (i < exchangePage.headerLinks.size()) {   //просто прокликиваем по всем видимым ссылкам и проверяем наличие хедера
-            exchangePage.headerLinks.get(i).click();
-            exchangePage.headerBlock.shouldBe(Condition.exist);
-            exchangePage.open();
+        exchangePage.isExistElement(Page.Header.header);
+        exchangePage.isExistElement(Page.Header.logo);
+         while (i < header.links.size()) {   //просто прокликиваем по всем видимым ссылкам и проверяем наличие хедера
+            header.links.get(i).click();
+            Page.Header.header.shouldBe(Condition.exist);
+            exchangePage.back();
             i++;
         }
-
         //нужно ещё прокликать скрытые
-
+        //разобраться с chromedriver и mouseMove
+       // System.out.println(exchangePage.headerHiddenLinks);
+       // exchangePage.moveToElement(exchangePage.headerAdditionalMenu);
     }
+
+
+    @Test
+    public void checkActiveLinkIsHighlighted() { //4. Проверить, что выделен раздел "Курсы валют"
+        Assert.assertTrue(exchangePage.isActive(header.highlightedLink));
+    }
+
+    @Test
+    public void checkFooterLinks() {
+        footer.links.get(0).shouldBe(Condition.visible);
+        System.out.println(footer.links.size());
+    }
+
 
 }
